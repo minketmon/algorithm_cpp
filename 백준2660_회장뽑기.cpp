9c29c6eb07ab75faaa1684f301e2e414d0ea2de9
minketmon <gjm1026@naver.com>
2021-12-15 16:32:39 +0900

    플로이드-워셜 회장뽑기

--- 백준2660_회장뽑기.cpp
+++ 백준2660_회장뽑기.cpp
@@ -1,30 +1,51 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 #define INF 123456789
 using namespace std;
 int n, dist[51][51];
+vector<pair<int, int>> score;
+vector<int> ans;
 
 int main() {
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
     cin >> n;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            dist[i][j] = INF;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i != j)dist[i][j] = INF;
         }
     }
     while (true) {
         int a, b;
         cin >> a >> b;
         if (a == -1 && b == -1) break;
-        dist[a][a] = 0;
         dist[a][b] = 1;
         dist[b][a] = 1;
     }
-
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cout << dist[i][j] << " ";
-        }cout << "\n";
+    // 플로이드
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
     }
+    for (int i = 1; i <= n; i++) {
+        int _max = -1;
+        for (int j = 1; j <= n; j++) {
+            _max = max(_max, dist[i][j]);
+        }
+        score.emplace_back(_max, i);
+    }
+    sort(score.begin(), score.end());
+    int _min = score[0].first;
+    for (int i = 0; i < n; i++) {
+        if (score[i].first == _min) ans.push_back(score[i].second);
+    }
+    sort(ans.begin(), ans.end());
+    cout << score[0].first << " " << ans.size() << "\n";
+    for (int a: ans) cout << a << " ";
 }
