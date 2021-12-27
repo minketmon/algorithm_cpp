6aa68a3fe8ac0ce9aff0f79ec6f05d8ad0e25c0d
minketmon <gjm1026@naver.com>
2021-12-27 18:41:33 +0900

    게리맨더링

--- 백준17471_게리맨더링.cpp
+++ 백준17471_게리맨더링.cpp
@@ -9,6 +9,49 @@ int _min = 123456789;
 vector<int> v[101];
 vector<int> a, b;
 
+void gerrymandering() {
+    queue<int> q;
+    q.push(a[0]);
+    visited2[a[0]] = true;
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        for (int next: v[cur]) {
+            bool flag = false;
+            for (auto a: a) if (next == a) flag = true;
+            if (!flag) continue;
+            if (visited2[next] == 1) continue;
+            visited2[next] = true;
+            q.push(next);
+        }
+    }
+    for (auto a: a)
+        if (visited2[a] == -1) return;
+    memset(visited2, -1, sizeof(visited2));
+    q.push(b[0]);
+    visited2[b[0]] = true;
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        for (int next: v[cur]) {
+            bool flag = false;
+            for (auto b: b) if (next == b) flag = true;
+            if (!flag) continue;
+            if (visited2[next] == 1) continue;
+            visited2[next] = true;
+            q.push(next);
+        }
+    }
+    for (auto b: b)
+        if (visited2[b] == -1) return;
+    int a_pop = 0, b_pop = 0;
+    for (auto a: a) a_pop += pop[a];
+    for (auto b: b) b_pop += pop[b];
+    int gap = abs(a_pop - b_pop);
+    _min = min(_min, gap);
+    return;
+}
+
 void combination(int idx, int cnt, int k) {
     if (cnt == k) {
         a.clear(), b.clear();
@@ -17,46 +60,7 @@ void combination(int idx, int cnt, int k) {
             if (visited[i] == true) a.push_back(i + 1);
             else b.push_back(i + 1);
         }
-        queue<int> q;
-        q.push(a[0]);
-        visited2[a[0]] = true;
-        while (!q.empty()) {
-            int cur = q.front();
-            q.pop();
-            for (int next: v[cur]) {
-                bool flag = false;
-                for (auto a: a) if (next == a) flag = true;
-                if (!flag) continue;
-                if (visited2[next] == 1) continue;
-                visited2[next] = true;
-                q.push(next);
-            }
-        }
-        for (auto a: a)
-            if (visited2[a] == -1) return;
-        memset(visited2, -1, sizeof(visited2));
-        q.push(b[0]);
-        visited2[b[0]] = true;
-        while (!q.empty()) {
-            int cur = q.front();
-            q.pop();
-            for (int next: v[cur]) {
-                bool flag = false;
-                for (auto b: b) if (next == b) flag = true;
-                if (!flag) continue;
-                if (visited2[next] == 1) continue;
-                visited2[next] = true;
-                q.push(next);
-            }
-        }
-        for (auto b: b)
-            if (visited2[b] == -1) return;
-        int a_pop = 0, b_pop = 0;
-        for (auto a: a) a_pop += pop[a];
-        for (auto b: b) b_pop += pop[b];
-        int gap = abs(a_pop - b_pop);
-        _min = min(_min, gap);
-        return;
+        gerrymandering();
     }
     for (int i = idx; i < n; i++) {
         if (visited[i] == true) continue;
