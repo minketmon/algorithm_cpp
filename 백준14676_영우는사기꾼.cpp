fbb314fc1547baca649bfeaa36dba15313598077
minketmon <gjm1026@naver.com>
2022-01-03 16:14:25 +0900

    영우는사기꾼

--- 백준14676_영우는사기꾼.cpp
+++ 백준14676_영우는사기꾼.cpp
@@ -1,18 +1,50 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 
 using namespace std;
-int n, m, k, in[100001];
+
+int n, m, k, in[100001], isbuild[100001];
+bool flag;
 vector<int> v[100001];
+
+void built(int num) {
+    if (in[num] <= 0) {
+        isbuild[num]++;
+        for (auto a: v[num]) in[a]--;
+    } else {
+        flag = true;
+    }
+}
+
+void destroy(int num) {
+    if (isbuild[num] <= 0) {
+        flag = true;
+    } else {
+        isbuild[num]--;
+        for (auto a: v[num]) in[a]++;
+    }
+}
 
 int main() {
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
+
     cin >> n >> m >> k;
     for (int i = 1; i <= m; i++) {
         int a, b;
         cin >> a >> b;
         v[a].push_back(b);
+        in[b]++;
     }
+    for (int i = 1; i <= k; i++) {
+        int a, b;
+        cin >> a >> b;
+        if (a == 1) { // 건설
+            built(b);
+        } else {
+            destroy(b);
+        }
+    }
+    if (flag) cout << "Lier!";
+    else cout << "King-God-Emperor";
 }
