5a37aba5e7f3fb069977a27b3617fad0cfdd81a3
minketmon <gjm1026@naver.com>
2022-01-18 23:06:45 +0900

    SWEA List

--- SAMSUNG/SWEA12304_기초DoubleLinkedList연습.cpp
+++ SAMSUNG/SWEA12304_기초DoubleLinkedList연습.cpp
@@ -1,12 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <iostream>
+#include <stdio.h>
 
 using namespace std;
 #define MAX_NODE 10000
 
 enum {
-    ADD_HEAD = 1, ADD_TAIL, ADD_NUM, REMOVE, PRINT, END = 99
+    ADD_HEAD = 1, ADD_TAIL, ADD_NUM, FIND, REMOVE, PRINT, PRINT_REVERSE, END = 99
 };
 
 extern void init();
@@ -17,14 +17,19 @@ extern void addNode2Tail(int data);
 
 extern void addNode2Num(int data, int num);
 
+extern int findNode(int data);
+
 extern void removeNode(int data);
 
 extern int getList(int output[MAX_NODE]);
+
+extern int getReversedList(int output[MAX_NODE]);
 
 #define MAX_NODE 10000
 
 struct Node {
     int data;
+    Node *prev;
     Node *next;
 };
 
@@ -34,6 +39,7 @@ Node *head;
 
 Node *getNode(int data) {
     node[nodeCnt].data = data;
+    node[nodeCnt].prev = nullptr;
     node[nodeCnt].next = nullptr;
     return &node[nodeCnt++];
 }
@@ -45,8 +51,16 @@ void init() {
 
 void addNode2Head(int data) {
     Node *new_node = getNode(data);
-    new_node->next = head->next;
-    head->next = new_node;
+    if (head->next == nullptr) {
+        new_node->next = head->next;
+        new_node->prev = head;
+        head->next = new_node;
+    } else {
+        new_node->next = head->next;
+        head->next->prev = new_node;
+        head->next = new_node;
+        new_node->prev = head;
+    }
 }
 
 void addNode2Tail(int data) {
@@ -54,6 +68,7 @@ void addNode2Tail(int data) {
     Node *temp = head;
     while (temp->next) temp = temp->next;
     temp->next = new_node;
+    new_node->prev = temp;
     new_node->next = nullptr;
 }
 
@@ -63,8 +78,27 @@ void addNode2Num(int data, int num) {
     for (int i = 0; i < num - 1; i++) {
         temp = temp->next;
     }
-    new_node->next = temp->next;
-    temp->next = new_node;
+    if (temp->next == nullptr) {
+        new_node->next = temp->next;
+        temp->next = new_node;
+        new_node->prev = temp;
+    } else {
+        new_node->next = temp->next;
+        temp->next->prev = new_node;
+        temp->next = new_node;
+        new_node->prev = temp;
+    }
+}
+
+int findNode(int data) {
+    Node *temp = head;
+    int cnt = 1;
+    while (temp->next) {
+        if (temp->next->data == data) break;
+        temp = temp->next;
+        cnt++;
+    }
+    return cnt;
 }
 
 void removeNode(int data) {
@@ -73,7 +107,12 @@ void removeNode(int data) {
         if (temp->next->data == data) break;
         temp = temp->next;
     }
-    if (temp->next != nullptr) temp->next = temp->next->next;
+    if (temp->next == nullptr) return;
+    temp = temp->next;
+    if (temp->next != nullptr) {
+        temp->next->prev = temp->prev;
+        temp->prev->next = temp->next;
+    } else temp->prev->next = temp->next;
 }
 
 int getList(int output[MAX_NODE]) {
@@ -87,32 +126,56 @@ int getList(int output[MAX_NODE]) {
     return cnt;
 }
 
+int getReversedList(int output[MAX_NODE]) {
+    Node *temp = head;
+    while (temp->next) {
+        temp = temp->next;
+    }
+    int cnt = 0;
+    while (temp->prev) {
+        output[cnt] = temp->data;
+        temp = temp->prev;
+        cnt++;
+    }
+    return cnt;
+}
+
 static void run() {
     while (1) {
-        int cmd, data, num, cnt, i;
+        int cmd, data, num, cnt, i = 0;
         int output[MAX_NODE] = {0};
 
         scanf("%d", &cmd);
         switch (cmd) {
-            case ADD_HEAD:
+            case ADD_HEAD: // 1
                 scanf("%d", &data);
                 addNode2Head(data);
                 break;
-            case ADD_TAIL:
+            case ADD_TAIL: // 2
                 scanf("%d", &data);
                 addNode2Tail(data);
                 break;
-            case ADD_NUM:
+            case ADD_NUM: // 3
                 scanf("%d %d", &data, &num);
                 addNode2Num(data, num);
                 break;
-            case REMOVE:
+            case FIND: // 4
+                scanf("%d", &data);
+                num = findNode(data);
+                printf("%d\n", num);
+                break;
+            case REMOVE: // 5
                 scanf("%d", &data);
                 removeNode(data);
                 break;
-            case PRINT:
+            case PRINT: // 6
                 cnt = getList(output);
-                i = 0;
+                while (cnt--)
+                    printf("%d ", output[i++]);
+                printf("\n");
+                break;
+            case PRINT_REVERSE: // 7
+                cnt = getReversedList(output);
                 while (cnt--)
                     printf("%d ", output[i++]);
                 printf("\n");
@@ -125,7 +188,7 @@ static void run() {
 
 int main(void) {
     //setbuf(stdout, NULL);
-    //freopen("sll_input.txt", "r", stdin);
+    //freopen("dll_input.txt", "r", stdin);
 
     int T;
     scanf("%d", &T);
