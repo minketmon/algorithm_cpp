9d3ac68e98597ae6ce580d2e26892b7054b9845a
gojeongmin <gjm1026@naver.com>
2022-05-23 17:29:06 +0900

    프린터

--- 프로그래머스_프린터.cpp
+++ 프로그래머스_프린터.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <queue>
 
 using namespace std;
@@ -8,23 +7,20 @@ using namespace std;
 bool findBigger(queue<pair<int, int>> q) {
     int top = q.front().first;
     while (!q.empty()) {
-        int temp = q.front().first;
+        if (top < q.front().first) return true;
         q.pop();
-        if (top < temp) return true;
     }
     return false;
 }
 
 int solution(vector<int> priorities, int location) {
-    int answer = 0, cnt = 0;
+    int cnt = 0;
     queue<pair<int, int>> q;
     for (int i = 0; i < priorities.size(); i++) q.push({priorities[i], i});
     while (!q.empty()) {
         if (findBigger(q)) {
-            int a = q.front().first;
-            int b = q.front().second;
+            q.push({q.front().first, q.front().second});
             q.pop();
-            q.push({a, b});
         } else {
             if (q.front().second == location) return ++cnt;
             q.pop();
