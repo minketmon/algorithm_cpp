dabe4263235015e5754463d619360e7f002f3c45
gojeongmin <gjm1026@naver.com>
2022-05-26 17:30:06 +0900

    카펫, 오픈채팅방

--- 프로그래머스_오픈채팅방.cpp
+++ 프로그래머스_오픈채팅방.cpp
@@ -2,54 +2,26 @@
 #include <vector>
 #include <map>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
 vector<string> solution(vector<string> record) {
     vector<string> answer;
+    vector<pair<string, string>> temp;
     map<string, string> m;
-    vector<pair<string, string>> p;
     for (int i = 0; i < record.size(); i++) {
-        if (record[i][0] == 'E') {
-            int j;
-            string id, nickname;
-            for (j = 6; record[i][j] != ' '; j++) {
-                id += record[i][j];
-            }
-            for (int k = j + 1; k < record[i].size(); k++) {
-                nickname += record[i][k];
-            }
-            m[id] = nickname;
-            p.push_back({"E", id});
-        } else if (record[i][0] == 'L') {
-            int j;
-            string id;
-            for (j = 6; j < record[i].size(); j++) {
-                id += record[i][j];
-            }
-            p.push_back({"L", id});
-        } else if (record[i][0] == 'C') {
-            int j;
-            string id, nickname;
-            for (j = 7; record[i][j] != ' '; j++) {
-                id += record[i][j];
-            }
-            for (int k = j + 1; k < record[i].size(); k++) {
-                nickname += record[i][k];
-            }
-            m[id] = nickname;
-        }
+        istringstream ss(record[i]);
+        string stringBuffer;
+        vector<string> x;
+        while (getline(ss, stringBuffer, ' ')) x.push_back(stringBuffer);
+        if (x[0] == "Enter") {
+            if (m.find(x[1]) != m.end()) m[x[1]] = x[2];
+            else m.insert({x[1], x[2]});
+            temp.push_back({x[1], "님이 들어왔습니다."});
+        } else if (x[0] == "Change") m[x[1]] = x[2];
+        else temp.push_back({x[1], "님이 나갔습니다."});
     }
-
-    for (int i = 0; i < p.size(); i++) {
-        string res;
-        res += m[p[i].second];
-        if (p[i].first == "E") {
-            res += "님이 들어왔습니다.";
-        } else if (p[i].first == "L") {
-            res += "님이 나갔습니다.";
-        }
-        answer.push_back(res);
-    }
+    for (auto a: temp) answer.push_back(m[a.first] + a.second);
     return answer;
 }
--- 프로그래머스_카펫.cpp
+++ 프로그래머스_카펫.cpp
@@ -8,11 +8,8 @@ vector<int> solution(int brown, int yellow) {
     vector<pair<int, int>> vec;
     vector<int> ans;
     int sz = brown + yellow;
-    for (int i = 1; i <= sqrt(sz); i++) {
-        if (sz % i == 0) vec.push_back({i, sz / i});
-    }
+    for (int i = 1; i <= sqrt(sz); i++) if (sz % i == 0) vec.push_back({sz / i, i});
     for (auto a: vec)
         if (a.first * 2 + a.second * 2 - 4 == brown)
-            if (a.first > a.second) return {a.first, a.second};
-            else return {a.second, a.first};
+            return {a.first, a.second};
 }
