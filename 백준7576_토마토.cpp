5829882e2a906c528a847083ee67637bd69f5003
gojeongmin <gjm1026@naver.com>
2022-06-07 00:47:58 +0900

    토마토 re

--- 백준7576_토마토.cpp
+++ 백준7576_토마토.cpp
@@ -49,3 +49,53 @@ int main() {
         cout << -1;
     }
 }
+
+/* 220607
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+int n, m, arr[1001][1001], visited[1001][1001], cnt, day;
+int _x[] = {-1, 1, 0, 0};
+int _y[] = {0, 0, -1, 1};
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    queue<pair<int, int>> q;
+    cin >> m >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+            if (arr[i][j] == 1) q.push({i, j});
+        }
+    }
+    while (!q.empty()) {
+        int size = q.size();
+        while (size--) {
+            int cx = q.front().first;
+            int cy = q.front().second;
+            q.pop();
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + _x[i], ny = cy + _y[i];
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                if (!arr[nx][ny]) {
+                    arr[nx][ny] = day + 1;
+                    q.push({nx, ny});
+                }
+            }
+        }
+        day++;
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (arr[i][j] == 0) {
+                cout << -1;
+                return 0;
+            }
+        }
+    }
+    cout << day - 1;
+}
+ */
