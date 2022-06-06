9ca22d196b10b43711b81e3a3ab32df580dfbed5
gojeongmin <gjm1026@naver.com>
2022-06-07 01:29:37 +0900

    ACM Craft re

--- 백준1005_ACMCraft.cpp
+++ 백준1005_ACMCraft.cpp
@@ -51,3 +51,48 @@ int main() {
         sol();
     }
 }
+
+/*200607
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
+
+using namespace std;
+int tc, n, k, w, t[1001], in[1001];
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin >> tc;
+    while(tc--) {
+        memset(in, 0, sizeof(in));
+        memset(t, 0, sizeof(t));
+        vector<int> vec[1001];
+        priority_queue<pair<int, int>> pq;
+        cin >> n >> k;
+        for (int i = 1; i <= n; i++) cin >> t[i];
+        for (int i = 0; i < k; i++) {
+            int a, b;
+            cin >> a >> b;
+            vec[a].push_back(b);
+            in[b]++;
+        }
+        cin >> w;
+        for (int i = 1; i <= n; i++) if (in[i] == 0) pq.push({-t[i], i});
+        while (!pq.empty()) {
+            int cur = pq.top().second;
+            int tt = -pq.top().first;
+            pq.pop();
+            if (cur == w) {
+                cout << tt << '\n';
+                break;
+            }
+            for (auto a: vec[cur]) {
+                in[a]--;
+                if (in[a] == 0) pq.push({-(tt + t[a]), a});
+            }
+        }
+    }
+}
+ *
