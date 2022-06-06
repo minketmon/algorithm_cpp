2dd0fcaaa8f4080b7ce7e466c9be2a73b65efefa
gojeongmin <gjm1026@naver.com>
2022-06-06 23:05:35 +0900

    최단경로 re

--- 백준1753_최단경로.cpp
+++ 백준1753_최단경로.cpp
@@ -1,43 +1,82 @@
-#include <iostream>
-#include <queue>
-#include <vector>
-#define INF 123456789
-using namespace std;
-int v, e, k;
+//#include <iostream>
+//#include <queue>
+//#include <vector>
+//#define INF 123456789
+//using namespace std;
+//int v, e, k;
+//
+//vector<pair<int, int>> edge[20001];
+//int dist[20001];
+//
+//int main() {
+//    cin.tie(nullptr);
+//    ios::sync_with_stdio(false);
+//
+//    cin >> v >> e >> k;
+//    for (int i = 0; i < e; i++) {
+//        int a, b, c;
+//        cin >> a >> b >> c;
+//        edge[a].push_back({b, c});
+//    }
+//    priority_queue<pair<int, int>> pq;
+//    for(int i=1; i<=v; i++){
+//        dist[i] = INF;
+//    }
+//    dist[k] = 0;
+//    pq.push({0, k});
+//    while(!pq.empty()){
+//        int weight = -pq.top().first;
+//        int vertex = pq.top().second;
+//        pq.pop();
+//        for(int i=0; i<edge[vertex].size(); i++){
+//            int next = edge[vertex][i].first;
+//            int total = weight + edge[vertex][i].second;
+//            if(dist[next] <= total) continue;
+//            dist[next] = total;
+//            pq.push({-total, next});
+//        }
+//    }
+//    for(int i=1; i<=v; i++ ){
+//        if(dist[i]==INF) cout << "INF\n";
+//        else cout << dist[i] << "\n";
+//    }
+//}
 
-vector<pair<int, int>> edge[20001];
-int dist[20001];
+#include <iostream>
+#include <vector>
+#include <queue>
+
+#define INF 987654321
+
+using namespace std;
+int v, e, k, dist[20001];
+vector<pair<int, int>> vec[20001];
 
 int main() {
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
-
     cin >> v >> e >> k;
+    for (int i = 1; i <= v; i++) dist[i] = INF;
     for (int i = 0; i < e; i++) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        edge[a].push_back({b, c});
+        int u, v, w;
+        cin >> u >> v >> w;
+        vec[u].push_back({v, w});
     }
     priority_queue<pair<int, int>> pq;
-    for(int i=1; i<=v; i++){
-        dist[i] = INF;
-    }
-    dist[k] = 0;
     pq.push({0, k});
-    while(!pq.empty()){
+    dist[k] = 0;
+    while (!pq.empty()) {
         int weight = -pq.top().first;
-        int vertex = pq.top().second;
+        int cur = pq.top().second;
         pq.pop();
-        for(int i=0; i<edge[vertex].size(); i++){
-            int next = edge[vertex][i].first;
-            int total = weight + edge[vertex][i].second;
-            if(dist[next] <= total) continue;
-            dist[next] = total;
-            pq.push({-total, next});
+        for (int i = 0; i < vec[cur].size(); i++) {
+            int next = vec[cur][i].first;
+            int d = weight + vec[cur][i].second;
+            if (dist[next] <= d) continue;
+            dist[next] = d;
+            pq.push({-d, next});
         }
     }
-    for(int i=1; i<=v; i++ ){
-        if(dist[i]==INF) cout << "INF\n";
-        else cout << dist[i] << "\n";
+    for (int i = 1; i <= v; i++) {
+        if (dist[i] == INF) cout << "INF\n";
+        else cout << dist[i] << '\n';
     }
 }
